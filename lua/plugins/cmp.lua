d2288f6a62a69c6766cecbd285ab1bc792aad0dc
= <=>
2024-10-01 11:14:07 -0700

    snippet mappings

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -17,6 +17,8 @@ return { -- override nvim-cmp plugin
       ["<C-b>"] = cmp.mapping.scroll_docs(-4),
       ["<C-f>"] = cmp.mapping.scroll_docs(4),
       ["<C-Space>"] = cmp.mapping.complete(),
+      ["<C-j>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Select },
+      ["<C-k>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Select },
       ["<C-e>"] = cmp.mapping.abort(),
       ["<CR>"] = cmp.mapping.confirm { behavior = cmp.ConfirmBehavior.Insert, select = true },
     }
