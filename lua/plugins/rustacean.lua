2c28833c983a02d48bf4155e15c679e33ff89ac6
Will <wjeffreys96@gmail.com>
2025-08-08 17:05:24 -0700

    ignore unused variables, install rustacean.nvim

---
+++ lua/plugins/rustacean.lua
@@ -0,0 +1,26 @@
+vim.g.rustaceanvim = {
+  -- Plugin configuration
+  tools = {},
+  -- LSP configuration
+  server = {
+    on_attach = function(client, bufnr)
+      -- you can also put keymaps in here
+    end,
+    default_settings = {
+      -- rust-analyzer language server configuration
+      ["rust-analyzer"] = {
+        cargo = {
+          allTargets = false,
+        },
+      },
+    },
+  },
+  -- DAP configuration
+  dap = {},
+}
+
+return {
+  'mrcjkb/rustaceanvim',
+  version = '^6',
+  lazy = false, -- This plugin is already lazy
+}
