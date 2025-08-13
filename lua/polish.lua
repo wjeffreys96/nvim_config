bd2d6bca9d6be3d3d6ca6d4b92dbc9aa4c5603df
Will <wjeffreys96@gmail.com>
2025-08-12 18:27:22 -0700

    cw -> ciw

--- lua/polish.lua
+++ lua/polish.lua
@@ -3,3 +3,6 @@ vim.opt.mouse = ""
 
 -- set pyenv venv
 vim.g.python3_host_prog = "/home/will/.pyenv/versions/neovim3/bin/python"
+
+-- Change the whole word regardless of if the cursor is at the beginning or not
+vim.keymap.set("n", "cw", "ciw")
