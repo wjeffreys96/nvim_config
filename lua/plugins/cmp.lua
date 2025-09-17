b16448c6640a7ac813285198ea8437545bfdb350
= <=>
2025-09-17 12:42:11 -0700

    fix tab indent bug

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -3,8 +3,5 @@ return {
   optional = true,
   opts = function(_, opts)
     if not opts.keymap then opts.keymap = {} end
-    opts.keymap["<Tab>"] = { "snippet_forward", "fallback" }
-    opts.keymap["<S-Tab>"] = { "snippet_backward", "fallback" }
-    opts.keymap["<C-l>"] = { "select_and_accept", "fallback" }
   end,
 }
